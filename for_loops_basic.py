9c1e3f12f15f12f71bd64d59ddd5ed4c1e36d9dc
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-25 17:18:57 -0700

    add 1. basic

--- for_loops_basic.py
+++ for_loops_basic.py
@@ -2,3 +2,22 @@
 # Subj: Coding Dojo > Python Stack > Python > Fundamentals: For Loops Basic I
 # By:   Virgilio D. Cabading Jr.    Created: October 25, 2021
 # ////////////////////////////////////////////////////////////////////////////
+
+#   //// FUNCTIONS ///////////////////////////////////////////////////////////
+
+def print_desc(description) :
+    print("////////////////////////////////////////////////////////////////////////////")
+    print(description)
+    print()
+
+#   //// MAIN EXECUTION SECTION //////////////////////////////////////////////
+
+# ********************************************************
+print_desc("1. Basic - Print all integers from 0 to 150")
+
+output = ""
+for idx in range(151):
+    output += f"{idx} "
+else:
+    print(output)
+
